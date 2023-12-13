c3f02971fa0bf78ceea4867b448f231db69a2bf2
Luigi Rauso <darjkzer@hotmail.it>
2023-12-13 12:51:50 +0100

    16 - exercise completed

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -9,7 +9,8 @@ const setupDb = async () => {
   await db.none(`
     CREATE TABLE planets(
       id SERIAL NOT NULL PRIMARY KEY,
-      name TEXT NOT NULL
+      name TEXT NOT NULL,
+      image TEXT 
     )
   `);
 
@@ -79,4 +80,20 @@ const deleteById = async (req: Request, res: Response) => {
   res.status(200).json({ msg: "The planet was deleted" });
 };
 
-export { getAll, getOneById, create, updateById, deleteById };
+// ADD IMAGE
+const createImage = async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+  const filename = req.file?.path;
+
+  if (filename) {
+    db.none("UPDATE planets SET image=$2 WHERE id=$1", [id, filename])
+    res.status(201).json({ msg: "Planet image uploaded successfully" })
+  } else {
+    res.status(400).json({ msg: "Planet image failed to upload" })
+  }
+
+
+}
+
+export { getAll, getOneById, create, updateById, deleteById, createImage };
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import "express-async-errors";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import multer from "multer";
 
 import {
   getAll,
@@ -9,12 +10,25 @@ import {
   create,
   updateById,
   deleteById,
+  createImage
 } from "./controllers/planets.js";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./uploads")
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+const upload = multer({ storage })
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use("/uploads", express.static("uploads"))
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -33,6 +47,9 @@ app.put("/api/planets/:id", updateById);
 
 // DELETE A PLANET BY ID
 app.delete("/api/planets/:id", deleteById);
+
+// ADD IMAGE
+app.post("/api/planets/:id/image", upload.single("image"), createImage)
 
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
